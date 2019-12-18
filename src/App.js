e551265cbb9b5fc9824f8c33c65352e219a1a92b
MDAkramSiddiqui <mohammadakram077@gmail.com>
2019-12-18 15:25:53 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>This is a Recipe App</div>
+   );
+}
+
+export default App;
